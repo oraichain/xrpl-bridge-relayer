3aa457c7c2272d914cb340ca4d37cff13858cdd5
trung2891 <trungnv280201@gmail.com>
2024-08-20 00:19:51 +0700

    feat: add cli for relayer

---
+++ packages/relayer/src/helper.ts
@@ -0,0 +1,75 @@
+import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
+import { stringToPath } from "@cosmjs/crypto";
+import { DirectSecp256k1HdWallet, OfflineSigner } from "@cosmjs/proto-signing";
+import crypto from "crypto";
+import readlineSync from "readline-sync";
+import { Wallet } from "xrpl";
+
+export type RetryOptions = {
+  retry?: number;
+  timeout?: number;
+  callback?: (retry: number) => void;
+};
+
+export type UserWallet = { address: string; client: SigningCosmWasmClient };
+
+export const decrypt = (password: string, val: string) => {
+  const hashedPassword = crypto.createHash("sha256").update(password).digest();
+  const encryptedText = Buffer.from(val, "base64");
+  const IV = encryptedText.subarray(0, 16);
+  const encrypted = encryptedText.subarray(16);
+  const decipher = crypto.createDecipheriv("aes-256-cbc", hashedPassword, IV);
+  return Buffer.concat([
+    decipher.update(encrypted),
+    decipher.final(),
+  ]).toString();
+};
+
+export const fetchRetry = async (
+  url: RequestInfo | URL,
+  opts: RequestInit & RetryOptions = {}
+) => {
+  let { retry = 3, callback, timeout = 10000, ...init } = opts;
+  init.signal = AbortSignal.timeout(timeout);
+  while (retry > 0) {
+    try {
+      return await fetch(url, init);
+    } catch (e) {
+      callback?.(retry);
+      retry--;
+      if (retry === 0) {
+        throw e;
+      }
+    }
+  }
+};
+
+export const decryptMnemonic = (encryptedMnemonic: string) => {
+  const password = readlineSync.question("enter passphrase:", {
+    hideEchoBack: true,
+  });
+  return decrypt(password, encryptedMnemonic);
+};
+
+export const getOraiSigner = async (): Promise<OfflineSigner | string> => {
+  try {
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(
+      process.env.MNEMONIC ?? decryptMnemonic(process.env.MNEMONIC_ENCRYPTED),
+      {
+        hdPaths: [stringToPath(process.env.HD_PATH || "m/44'/118'/0'/0/0")],
+        prefix: "orai",
+      }
+    );
+
+    return wallet;
+  } catch (error: any) {
+    console.log({ error: error.message });
+    return "Error: " + error.message;
+  }
+};
+
+export const getXRPLWallet = (): Wallet => {
+  return Wallet.fromSeed(
+    process.env.XRPL_SEED || decryptMnemonic(process.env.XRPL_SEED_ENCRYPTED)
+  );
+};
--- packages/relayer/src/index.ts
+++ packages/relayer/src/index.ts
@@ -1,63 +1,19 @@
-import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
-import { OfflineSigner } from "@cosmjs/proto-signing";
-import { GasPrice } from "@cosmjs/stargate";
-import { ORAI } from "@oraichain/common";
-import {
-  CwXrplClient,
-  CwXrplInterface,
-} from "@oraichain/xrpl-bridge-contracts-sdk";
-import { setTimeout } from "timers/promises";
-import { Client, Wallet } from "xrpl";
-import { PROCESS_INTERVAL } from "./constants";
-import { RelayerAction, XrplClient } from "./type";
+#!/usr/bin/env -S node --no-warnings
 
-export default class XrplBridgeRelayer {
-  private relayerActions: RelayerAction[];
+import dotenv from "dotenv";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import relay from "./relayer";
 
-  constructor(
-    public readonly cwClient: CwXrplInterface,
-    public readonly xrplClient: XrplClient
-  ) {}
-
-  withRelayerActions(actions: RelayerAction[]) {
-    this.relayerActions = actions;
-    return this;
-  }
-
-  static async connect(
-    oraiRpc: string,
-    signer: OfflineSigner,
-    xrplBridgeAddress: string,
-    xrplClient: Client,
-    xrplWallet: Wallet
-  ) {
-    // const oraichain side
-    const cosmwasmClient = await SigningCosmWasmClient.connectWithSigner(
-      oraiRpc,
-      signer,
-      { gasPrice: GasPrice.fromString(`0.001${ORAI}`) }
-    );
-    const sender = await signer.getAccounts();
-
-    const oraiClient = new CwXrplClient(
-      cosmwasmClient,
-      sender[0].address,
-      xrplBridgeAddress
-    );
-
-    return new XrplBridgeRelayer(oraiClient, {
-      wallet: xrplWallet,
-      client: xrplClient,
-      relayerAddr: xrplWallet.address,
-    });
-  }
-
-  async relay() {
-    while (true) {
-      for (const relayerAction of this.relayerActions) {
-        await relayerAction.takeAction();
-      }
-      await setTimeout(PROCESS_INTERVAL);
-    }
-  }
-}
+yargs(hideBin(process.argv))
+  .scriptName("oraclePriceFeeds")
+  .config("env", (path) => {
+    return dotenv.config({ path }).parsed ?? {};
+  })
+  .default("env", ".env")
+  .command("start", "start relay between Oraichain and XRPL", relay)
+  .option("help", {
+    alias: "h",
+    demandOption: false,
+  })
+  .parse();
